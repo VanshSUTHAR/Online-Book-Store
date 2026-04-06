5ff516c639d110f637a4608992a935c90708f864
VanshSUTHAR <sutharvansh022@gmail.com>
2026-04-06 17:44:15 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-const API_URL = process.env.REACT_APP_API_URL || "https://online-books-0acj.onrender.com";
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const api = axios.create({
   baseURL: API_URL + "/api",
