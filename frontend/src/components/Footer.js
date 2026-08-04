fcf017b3388e145777ad949e4273ffa76e5579ac
VanshSUTHAR <sutharvansh022@gmail.com>
2026-08-04 15:40:24 +0530

    update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -212,7 +212,7 @@ export default function Footer() {
 
                 <span>Made by </span>
                 <a
-                  href="https://devstudiooffical.netlify.app/"
+                  href="vanshsuthar022.netlify.app/"
                   target="_blank"
                   rel="noopener noreferrer"
                   style={{
